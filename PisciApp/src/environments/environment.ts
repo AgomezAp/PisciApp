12f524fb5f80612391369444b74a989ee03238af
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2026-05-11 16:33:51 -0500

    Cambios environment

--- PisciApp/src/environments/environment.ts
+++ PisciApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lecturascartastarotcolombia.com/',
+  apiUrl: 'https://api.lecturascartastarotcolombia.com/',
 };
 
 /* export const environment = {
